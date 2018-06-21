dc76467cd64a70669927ca8e1bc29c798e0aa7f4
Bartlomiej Skwira <BartlomiejSkwira@users.noreply.github.com>
2018-06-21 10:54:33 +0200

    Jokester: fetch 10 random jokes with actions

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id='app'>
-    Got jokes?
-    {{$store.state}}
+    <h3>Got jokes?</h3>
+    <button class='btn btn-primary' @click='initJokes'>Get 10 jokes</button>
+    <div>
+      {{$store.state}}
+    </div>
   </div>
 </template>
+
+<script>
+import  { mapActions } from 'vuex'
+
+export default {
+  methods: mapActions(['initJokes'])
+}
+</script>
