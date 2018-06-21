e4dcc396cc680e462e727b569d9e2d81350dcf02
Bartlomiej Skwira <BartlomiejSkwira@users.noreply.github.com>
2018-06-21 11:02:06 +0200

    Jokester: add Joke component with minimal styling

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -2,16 +2,26 @@
   <div id='app'>
     <h3>Got jokes?</h3>
     <button class='btn btn-primary' @click='initJokes'>Get 10 jokes</button>
-    <div>
-      {{$store.state}}
+    <br />
+    <div class='col-md-12'>
+      <Joke
+        v-for="(joke, index) in $store.state.jokes"
+        :joke="joke"
+        key="key"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import  { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
-  methods: mapActions(['initJokes'])
+  methods: mapActions(['initJokes']),
+
+  components: {
+    Joke
+  }
 }
 </script>
